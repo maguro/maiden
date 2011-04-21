51591afe2fb82ed1fe97249c47a71ce9ff22d57a
Alan D. Cabrera <adc@toolazydogs.com>
2011-04-21 08:12:03 -0700

    Comments and logging

--- agent/src/main/java/com/toolazydogs/maiden/agent/asm/BeginEndMethodVisitor.java
+++ agent/src/main/java/com/toolazydogs/maiden/agent/asm/BeginEndMethodVisitor.java
@@ -19,6 +19,7 @@ package com.toolazydogs.maiden.agent.asm;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.objectweb.asm.AnnotationVisitor;
@@ -32,7 +33,17 @@ import org.objectweb.asm.tree.MethodNode;
 
 
 /**
- * A
+ * Use this class to insert code at the beginning and end of a method.
+ * <p/>
+ * An instance of {@link MethodVisitor} that wraps a method's code with a
+ * <code>try/finally</code> pair.  It calls registered listeners, which
+ * implement {@link BeginEndMethodListener}, before the try block to allow
+ * listeners to insert code to be executed before the method's original
+ * code.  When the <code>finally</code> segment is being generated the
+ * listeners are again called to insert code to be executed before the thread
+ * of control leaves the method.
+ * <p/>
+ * Listeners are also notified when a line number is encountered.
  */
 public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
 {
@@ -55,9 +66,22 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
         assert name != null;
         assert desc != null;
 
+        if (LOGGER.isLoggable(Level.CONFIG))
+        {
+            LOGGER.config("visitor: " + visitor);
+            LOGGER.config("access: " + access);
+            LOGGER.config("name: " + name);
+            LOGGER.config("desc: " + desc);
+            LOGGER.config("signature: " + signature);
+            for (String exception : exceptions) LOGGER.config("exception: " + exception);
+        }
+
         this.visitor = visitor;
 
+        // this MethodNode instance is used to fill in hte gaps of frames and maxes
         methodNode = new MethodNode(access, name, desc, signature, exceptions);
+
+        // We use this instance to keep track of local variables that may need to be created.
         lvs = new LocalVariablesSorter(access, desc, methodNode);
     }
 
@@ -104,7 +128,8 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
 
     public void visitInsn(int opcode)
     {
-        LOGGER.entering(CLASS_NAME, "visitInsn", new Object[]{opcode});
+        LOGGER.entering(CLASS_NAME, "visitInsn", opcode);
+
         flush();
 
         switch (opcode)
@@ -125,7 +150,6 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
 
                 for (BeginEndMethodListener listener : getReversed()) listener.end(lvs);
 
-
                 break;
 
             default:
@@ -139,9 +163,13 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
 
     public void visitIntInsn(int opcode, int operand)
     {
+        LOGGER.entering(CLASS_NAME, "visitIntInsn", new Object[]{opcode, operand});
+
         flush();
         sawCode = true;
         lvs.visitIntInsn(opcode, operand);
+
+        LOGGER.exiting(CLASS_NAME, "visitIntInsn");
     }
 
     public void visitVarInsn(int opcode, int var)
@@ -157,108 +185,163 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
 
     public void visitTypeInsn(int opcode, String type)
     {
+        LOGGER.entering(CLASS_NAME, "visitTypeInsn", new Object[]{opcode, type});
+
         flush();
         sawCode = true;
         lvs.visitTypeInsn(opcode, type);
+
+        LOGGER.exiting(CLASS_NAME, "visitTypeInsn");
     }
 
     public void visitFieldInsn(int opcode, String owner, String name, String desc)
     {
+        LOGGER.entering(CLASS_NAME, "visitFieldInsn", new Object[]{opcode, owner, name, desc});
+
         flush();
         sawCode = true;
         lvs.visitFieldInsn(opcode, owner, name, desc);
+
+        LOGGER.exiting(CLASS_NAME, "visitFieldInsn");
     }
 
     public void visitMethodInsn(int opcode, String owner, String name, String desc)
     {
+        LOGGER.entering(CLASS_NAME, "visitMethodInsn", new Object[]{opcode, owner, name, desc});
+
         flush();
         sawCode = true;
         lvs.visitMethodInsn(opcode, owner, name, desc);
+
+        LOGGER.exiting(CLASS_NAME, "visitMethodInsn");
     }
 
     public void visitJumpInsn(int opcode, Label label)
     {
+        LOGGER.entering(CLASS_NAME, "visitJumpInsn", new Object[]{opcode, label});
+
         flush();
         sawCode = true;
         lvs.visitJumpInsn(opcode, label);
+
+        LOGGER.exiting(CLASS_NAME, "visitJumpInsn");
     }
 
     public void visitLabel(Label label)
     {
-        if (state == State.NEED_START_LABEL)
-        {
-            start = new Label();
-            lvs.visitLabel(start);
-            state = State.CLEARED;
-        }
+        LOGGER.entering(CLASS_NAME, "visitLabel", label);
+
+        flush();
         lvs.visitLabel(label);
+
+        LOGGER.exiting(CLASS_NAME, "visitLabel");
     }
 
     public void visitLdcInsn(Object cst)
     {
+        LOGGER.entering(CLASS_NAME, "visitLdcInsn", cst);
+
         flush();
         sawCode = true;
         lvs.visitLdcInsn(cst);
+
+        LOGGER.exiting(CLASS_NAME, "visitLdcInsn");
     }
 
     public void visitIincInsn(int var, int increment)
     {
+        LOGGER.entering(CLASS_NAME, "visitIincInsn", new Object[]{var, increment});
+
         flush();
         sawCode = true;
         lvs.visitIincInsn(var, increment);
+
+        LOGGER.exiting(CLASS_NAME, "visitIincInsn");
     }
 
     public void visitTableSwitchInsn(int min, int max, Label dflt, Label[] labels)
     {
+        LOGGER.entering(CLASS_NAME, "visitTableSwitchInsn", new Object[]{min, max, dflt, labels});
+
         flush();
         sawCode = true;
         lvs.visitTableSwitchInsn(min, max, dflt, labels);
+
+        LOGGER.exiting(CLASS_NAME, "visitTableSwitchInsn");
     }
 
     public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels)
     {
+        LOGGER.entering(CLASS_NAME, "visitLookupSwitchInsn", new Object[]{dflt, keys, labels});
+
         flush();
         sawCode = true;
         lvs.visitLookupSwitchInsn(dflt, keys, labels);
+
+        LOGGER.exiting(CLASS_NAME, "visitLookupSwitchInsn");
     }
 
     public void visitMultiANewArrayInsn(String desc, int dims)
     {
+        LOGGER.entering(CLASS_NAME, "visitMultiANewArrayInsn", new Object[]{desc, dims});
+
         flush();
         sawCode = true;
         lvs.visitMultiANewArrayInsn(desc, dims);
+
+        LOGGER.exiting(CLASS_NAME, "visitMultiANewArrayInsn");
     }
 
     public void visitTryCatchBlock(Label start, Label end, Label handler, String type)
     {
+        LOGGER.entering(CLASS_NAME, "visitTryCatchBlock", new Object[]{start, end, handler, type});
+
         flush();
         lvs.visitTryCatchBlock(start, end, handler, type);
+
+        LOGGER.exiting(CLASS_NAME, "visitTryCatchBlock");
     }
 
     public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index)
     {
+        LOGGER.entering(CLASS_NAME, "visitLocalVariable", new Object[]{name, desc, signature, start, end, index});
+
         flush();
         lvs.visitLocalVariable(name, desc, signature, start, end, index);
+
+        LOGGER.exiting(CLASS_NAME, "visitLocalVariable");
     }
 
     public void visitLineNumber(int line, Label start)
     {
+        LOGGER.entering(CLASS_NAME, "visitLineNumber", new Object[]{line, start});
+
         flush();
         for (BeginEndMethodListener listener : listeners) listener.line(line);
         lvs.visitLineNumber(line, start);
+
+        LOGGER.exiting(CLASS_NAME, "visitLineNumber");
     }
 
     public void visitMaxs(int maxStack, int maxLocals)
     {
+        LOGGER.entering(CLASS_NAME, "visitMaxs", new Object[]{maxStack, maxLocals});
+
         flush();
+
+        LOGGER.exiting(CLASS_NAME, "visitMaxs");
     }
 
     public void visitEnd()
     {
+        LOGGER.entering(CLASS_NAME, "visitEnd");
+
         flush();
 
         if (sawCode)
         {
+            LOGGER.finest("Saw code");
+
             final Label l13 = new Label();
             final int local = lvs.newLocal(Type.getType(Throwable.class));
 
@@ -277,16 +360,28 @@ public class BeginEndMethodVisitor implements MethodVisitor, Opcodes
         lvs.visitEnd();
 
         methodNode.accept(visitor);
+
+        LOGGER.exiting(CLASS_NAME, "visitEnd");
     }
 
+    /**
+     * Delay flushing out first label for our try/finally block until the first
+     * bit of code is encountered.
+     */
     private void flush()
     {
+        LOGGER.entering(CLASS_NAME, "flush");
+
         if (state == State.NEED_START_LABEL)
         {
+            LOGGER.finest("Need start label");
+
             start = new Label();
             lvs.visitLabel(start);
             state = State.CLEARED;
         }
+
+        LOGGER.exiting(CLASS_NAME, "flush");
     }
 
     private static enum State
